375487aab09b1187bee224aacf69230074d69eb8
pacbac <jclaytonc99@gmail.com>
2018-05-20 13:22:48 -0700

    Fixed access token error bug

--- backend/home/utils/authTools.py
+++ backend/home/utils/authTools.py
@@ -2,9 +2,6 @@ import spotipy, spotipy.util as util
 from .. import clientinfo as ci
 
 def getSpotify(spotify):
-    #if exists already, just return it
-    if spotify: return spotify
-    #else, create new spotify obj
     authObj = spotipy.oauth2.SpotifyClientCredentials(client_id=ci.client_id, client_secret=ci.client_secret)
     token = authObj.get_access_token()
     return spotipy.Spotify(auth=token)
